f99244ca14e3bce67653d6aad796328bb57adfd0
abirbhattacharya82 <abirbhattacharya82@gmail.com>
2022-04-03 17:33:52 +0530

    Upload Page

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,15 +1,12 @@
 const show_game=()=>{
     document.getElementById('game').style.display="block";
-    document.getElementById('upload').style.display="none";
     document.getElementById('developer').style.display="none";
 }
 const show_upload=()=>{
     document.getElementById('game').style.display="none";
-    document.getElementById('upload').style.display="block";
     document.getElementById('developer').style.display="none";
 }
 const show_developer=()=>{
     document.getElementById('game').style.display="none";
-    document.getElementById('upload').style.display="none";
     document.getElementById('developer').style.display="block";
 }
